154e0418b35eda7012ff147c4296b212775a11b4
Mark <m.m.dekker@outlook.com>
2016-12-09 16:00:10 +0100

    LAPTOP

--- FortranReader.py
+++ FortranReader.py
@@ -13,13 +13,13 @@ f0=np.sin(phi)*2.*Omega
 R=287.
 
 # DATA READ
-#file1 = './mean_field_Co100.dat'
-file2 = './evol_field_Co350.dat'
+file1 = './mean_field_Co175_L0.dat'
+file2 = './evol_field_Co175_L0.dat'
 
-#open1=open(file1,'r')
+open1=open(file1,'r')
 open2=open(file2,'r')
 
-#read1=open1.readlines()
+read1=open1.readlines()
 read2=open2.readlines()
 
 # TIME EVOLUTION
@@ -42,26 +42,10 @@ VdT_o=np.transpose(VdT_o).astype(np.float)
 times=np.array(time).astype(np.float)
 
 # MEAN FIELD
-#Mpsi_a=read1[0].split()[1:11]
-#Mtheta_a=read1[0].split()[11:21]
-#Mpsi_o=read1[0].split()[21:29]
-#MdT_o=read1[0].split()[29:38]
-
-#%%std, Co100
-V1_1=Vpsi_a
-V1_2=Vpsi_o
-V1_3=Vtheta_a
-V1_4=VdT_o
-#%%std, Co200
-V2_1=Vpsi_a
-V2_2=Vpsi_o
-V2_3=Vtheta_a
-V2_4=VdT_o
-#%%std, Co350
-V3_1=Vpsi_a
-V3_2=Vpsi_o
-V3_3=Vtheta_a
-V3_4=VdT_o
+Mpsi_a=read1[0].split()[1:11]
+Mtheta_a=read1[0].split()[11:21]
+Mpsi_o=read1[0].split()[21:29]
+MdT_o=read1[0].split()[29:38]
 
 #%%d1e9
 V1_1=Vpsi_a
